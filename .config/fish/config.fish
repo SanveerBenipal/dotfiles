9bf1955f198a9898328e2861b13dc97c09590483
Sanveer Singh <s.sanveerbenipal@gmail.com>
2025-09-01 20:13:05 +1000

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    if command -q atuin
+       atuin init fish --disable-up-arrow | source
+    end
+    if test -f /usr/share/autojump/autojump.fish
+        source /usr/share/autojump/autojump.fish
+    end
+end
+
+if test -f "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
+
+if test -f "$HOME/.homesick/repos/homeshick/homeshick.fish"
+    source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+end
+
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+
+string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
